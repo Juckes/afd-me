4467809495c7400088febab087eca30fdc366506
Michael <michaeljuckes@hotmail.com>
2023-12-06 14:45:23 +0000

    test repo conn

--- frontdoor-profile.tf
+++ frontdoor-profile.tf
@@ -1,10 +1,10 @@
 # Taken from afd-module
 resource "azurerm_cdn_frontdoor_profile" "profile" {
-  name                = var.name # link this to the environment too so read dev.frontdoor...
+  name = var.name # link this to the environment too so read dev.frontdoor...
 
   resource_group_name = var.resource_group_name
   # resource_group_name = data.azurerm_resource_group.current.name
-  sku_name            = var.sku_name # sku_name = "${title(var.tier)}_AzureFrontDoor"
+  sku_name = var.sku_name # sku_name = "${title(var.tier)}_AzureFrontDoor"
 
   tags = merge(
     local.common_tags,
